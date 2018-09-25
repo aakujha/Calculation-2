363408485fe6107d19edfe5fccdf5e6aa81cb9f0
NEWLAB-12\dipesh <invisiblesoul91@gmail.com>
2018-09-25 10:25:42 +0545

    initial commit implemented and tested add function

---
+++ Calculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 10;
+            double sum = add(a, b);
+            Console.WriteLine("sum ={0}", sum);
+        }
+
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
