33945c373a460d67491c463e11b86ebba7877449
NEWLAB-12\dipesh <invisiblesoul91@gmail.com>
2018-09-27 09:41:47 +0545

    implemented multiply function

--- Calculation/Program.cs
+++ Calculation/Program.cs
@@ -13,12 +13,19 @@ namespace Calculation
             double a = 5;
             double b = 10;
             double sum = add(a, b);
+            double product = multiply(a, b);
             Console.WriteLine("sum ={0}", sum);
+            Console.WriteLine("product ={0}", product);
         }
 
         static double add(double a, double b)
         {
             return a + b;
+        }
+
+        static double multiply(double a, double b)
+        {
+            return a * b;
         }
     }
 }
